e66a15d5ab1aadcdc5fe96ed8f972ea938953079
unknown <afaizi1999@gmail.com>
2020-10-19 14:04:37 +0700

    My learning progress with net-ninja

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { motion } from "framer-motion";
 
-const Order = ({ pizza }) => {
+const containerVariants = {
+  hidden: { x: "100vw", opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    transition: {
+      ease: "easeInOut",
+    },
+  },
+};
+
+const childrenVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 4000);
+  }, [setShowModal]);
+
   return (
-    <div className="container order">
+    <motion.div
+      className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+      <motion.p variants={childrenVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childrenVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default Order;
